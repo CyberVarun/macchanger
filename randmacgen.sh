c89c5f86d1521b2050fd93b1e92244e3e0a891ad
CyberVarun <varunjagtap424@gmail.com>
2022-06-29 10:38:27 +0530

    modified syntax.

--- randmacgen.sh
+++ randmacgen.sh
@@ -8,10 +8,9 @@ then
 fi
 
 OUIMAC=$(shuf -n 1 ${FILE} | awk '{ print $3 }')
-UAAMAC=$(printf '%02x:%02x:%02x' $[RANDOM%256] $[RANDOM%256] $[RANDOM%256])
+UAAMAC=$(printf '%02x:%02x:%02x' "${RANDOM%256}" "${RANDOM%256}" "${RANDOM%256}")
 
 echo -e -n "Enter Network Interface Name: "
-read INFACE
+read -r INFACE
 
-macchanger -m "${OUIMAC}:${UAAMAC}" ${INFACE}
-
+macchanger -m "${OUIMAC}:${UAAMAC}" "${INFACE}"
